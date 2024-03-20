0972171c41e7ae60f98e86edaecac3e4f4a1e6a2
一闪一闪亮晶晶 <resetsix223@gmail.com>
2024-03-20 21:57:26 +0800

    fix: 修复@monaco-editor/react SQL 提示不生效问题

--- craco.config.js
+++ craco.config.js
@@ -1,41 +1,81 @@
-import EditorWorker from "../../node_modules/monaco-editor/esm/vs/editor/editor.worker?worker";
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const webpack = require("webpack");
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
+const process = require("process");
 
-import FlinkSQLWorker from "monaco-sql-languages/out/esm/flinksql/flinksql.worker?worker";
-import SparkSQLWorker from "monaco-sql-languages/out/esm/sparksql/sparksql.worker?worker";
-import HiveSQLWorker from "monaco-sql-languages/out/esm/hivesql/hivesql.worker?worker";
-import PGSQLWorker from "monaco-sql-languages/out/esm/pgsql/pgsql.worker?worker";
-import MySQLWorker from "monaco-sql-languages/out/esm/mysql/mysql.worker?worker";
-import TrinoSQLWorker from "monaco-sql-languages/out/esm/trinosql/trinosql.worker?worker";
-import ImpalaSQLWorker from "monaco-sql-languages/out/esm/impalasql/impalasql.worker?worker";
+const monacoWebpackPlugin = new MonacoWebpackPlugin({
+    // features: [],
+    // languages: [],
+    customLanguages: [
+        {
+            label: "mysql",
+            entry: "monaco-sql-languages/out/esm/mysql/mysql.contribution",
+            worker: {
+                id: "monaco-sql-languages/out/esm/mysql/mySQLWorker",
+                entry: "monaco-sql-languages/out/esm/mysql/mysql.worker",
+            },
+        },
+        {
+            label: "flinksql",
+            entry: "monaco-sql-languages/out/esm/flinksql/flinksql.contribution",
+            worker: {
+                id: "monaco-sql-languages/out/esm/flinksql/flinkSQLWorker",
+                entry: "monaco-sql-languages/out/esm/flinksql/flinksql.worker",
+            },
+        },
+        {
+            label: "sparksql",
+            entry: "monaco-sql-languages/out/esm/sparksql/sparksql.contribution",
+            worker: {
+                id: "monaco-sql-languages/out/esm/sparksql/sparkSQLWorker",
+                entry: "monaco-sql-languages/out/esm/sparksql/sparksql.worker",
+            },
+        },
+        {
+            label: "hivesql",
+            entry: "monaco-sql-languages/out/esm/hivesql/hivesql.contribution",
+            worker: {
+                id: "monaco-sql-languages/out/esm/hivesql/hiveSQLWorker",
+                entry: "monaco-sql-languages/out/esm/hivesql/hivesql.worker",
+            },
+        },
+        {
+            label: "trinosql",
+            entry: "monaco-sql-languages/out/esm/trinosql/trinosql.contribution",
+            worker: {
+                id: "monaco-sql-languages/out/esm/trinosql/TrinoSQLWorker",
+                entry: "monaco-sql-languages/out/esm/trinosql/trinosql.worker",
+            },
+        },
+        {
+            label: "pgsql",
+            entry: "monaco-sql-languages/out/esm/pgsql/pgsql.contribution",
+            worker: {
+                id: "monaco-sql-languages/out/esm/pgsql/PgSQLWorker",
+                entry: "monaco-sql-languages/out/esm/pgsql/pgsql.worker",
+            },
+        },
+        {
+            label: "impalasql",
+            entry: "monaco-sql-languages/out/esm/impalasql/impalasql.contribution",
+            worker: {
+                id: "monaco-sql-languages/out/esm/impalasql/impalaSQLWorker",
+                entry: "monaco-sql-languages/out/esm/impalasql/impalasql.worker",
+            },
+        },
+    ],
+});
 
-// eslint-disable-next-line no-restricted-globals
-self.MonacoEnvironment = {
-    getWorker(_, label) {
-        if (label === "flinksql") {
-            return new FlinkSQLWorker();
-        }
-        if (label === "hivesql") {
-            return new HiveSQLWorker();
-        }
-        if (label === "sparksql") {
-            return new SparkSQLWorker();
-        }
-        if (label === "pgsql") {
-            return new PGSQLWorker();
-        }
-        if (label === "mysql") {
-            return new MySQLWorker();
-        }
-        if (label === "trinosql") {
-            return new TrinoSQLWorker();
-        }
-        if (label === "impalasql") {
-            return new ImpalaSQLWorker();
-        }
-        return new EditorWorker();
-    },
-};
-
+// eslint-disable-next-line no-undef
 module.exports = {
-   
+    webpack: {
+        plugins: [
+            new webpack.DefinePlugin({
+                "process.env.NODE_DEBUG": process.env.NODE_DEBUG,
+            }),
+            monacoWebpackPlugin,
+        ],
+    },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import Editor from "@monaco-editor/react";
+import Editor, { loader } from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
+// import MonacoEditor from "react-monaco-editor";
+
+// 导入monaco-editor的语言支持
+loader.config({ monaco });
 
 function App() {
-    return <Editor height="500px" language="mysql" />;
+    // return <MonacoEditor height={600} language="mysql" theme="vs-dark" />;
+    return <Editor height={600} language="mysql" theme="vs-dark" />;
 }
 
 export default App;
